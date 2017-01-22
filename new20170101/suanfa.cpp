cce2d2c2a4b034be7c71081f864e33465e2fa499
Neo He <neohe2007@gmail.com>
2017-01-22 23:44:08 +0800

    construct binary tree

--- new20170101/suanfa.cpp
+++ new20170101/suanfa.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "suanfa.hpp"
+#include <stdexcept>
 
 int b_search(int a[], int n, int v)
 {
@@ -116,4 +117,48 @@ void MergeSort::merge_sort(int a[], int first, int middle, int last, int tmp[])
     for (int i = 0; i < k; k++) {
         a[first + i] = tmp[i];
     }
+}
+
+
+TreeNode *ClassTree::construct_tree(int *preOrder, int *inOrder, int length)
+{
+    if (preOrder == NULL || inOrder == NULL || length <= 0) {
+        return NULL;
+    }
+    
+    return consrtuct_tree_core(preOrder, preOrder + length - 1, inOrder, inOrder + length - 1);
+}
+
+TreeNode *ClassTree::consrtuct_tree_core(int *startPreOrder, int *endPreOrder, int *startInOrder, int *endInOrder)
+{
+    int rootValue = startPreOrder[0];   // 前序遍历的第一个是根结点
+    TreeNode *root = new TreeNode;
+    root->m_nValue = rootValue;
+    root->m_pLeft = root->m_pRight = NULL;
+    
+    if (startPreOrder == endPreOrder) {
+        if (*startPreOrder == *endPreOrder) {
+            return root;
+        }// else {
+         //   throw std::exception("Invalid input.");
+        //}
+    }
+    
+    int *rootInorder = startPreOrder;
+    while (rootInorder <= endInOrder && *rootInorder != rootValue) {
+        rootInorder++;
+    }
+    
+    int leftLength = rootInorder - startInOrder;
+    int *leftPreorderEnd = startPreOrder + leftLength;
+    
+    if (leftLength > 0) {
+        root->m_pLeft = consrtuct_tree_core(startPreOrder + 1, leftPreorderEnd, startInOrder, rootInorder - 1);
+    }
+    
+    if (leftLength < endPreOrder - startPreOrder) {
+        root->m_pRight = consrtuct_tree_core(leftPreorderEnd + 1, endPreOrder, rootInorder + 1, endInOrder);
+    }
+    
+    return root;
 }
